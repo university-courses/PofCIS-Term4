d8f00200421b56ae836291a0f4a72cc8068bbcd7
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-09-12 01:29:06 +0300

    add basic project structure

---
+++ Task1CS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1CS
+{
+	internal static class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+		}
+	}
+}
---
+++ TestTask1CS/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TestTask1CS
+{
+	public class Tests
+	{
+		[Fact]
+		public void Test()
+		{
+			Assert.True(true);
+		}
+	}
+}
