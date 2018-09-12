0163cade59c59a9ba4ad3681ed16a087a643c48d
dana_hrechyn <dana.hrechyn@gmail.com>
2018-09-12 23:27:19 +0300

    Implemented interfaces: IShape and IFileManager.

---
+++ Task1CS/Interfaces/IFileManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1CS.Interfaces
+{
+    public interface IFileManager
+    {
+        void WriteFile(string fileName);
+        void ReadFile(string fileName);
+    }
+}
