02c1caa6da044660b3edf521d24f96a6bc93af5d
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-09-13 18:17:07 +0300

    update class Point

--- Task1CS/Classes/Point.cs
+++ Task1CS/Classes/Point.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Task1CS.Classes
 {
     public struct Point
     {
-        public double X { get; set; }
-        public double Y { get; set; }
+        public double X { get; private set; }
+        public double Y { get; private set; }
 
+        public Point(string x, string y)
+        {
+            X = _stringToDouble(x);
+            Y = _stringToDouble(y);
+        }
+
+        private static double _stringToDouble(string strNum)
+        {
+            if (!double.TryParse(strNum, out var doubleNum))
+            {
+                throw new InvalidDataException("string contains non-number chars");
+            }
+
+            return doubleNum;
+        }
+        
+        public static double CalcDistance(Point first, Point second)
+        {
+            return Math.Sqrt(Math.Pow(first.X - second.X, 2) + Math.Pow(first.Y - second.Y, 2));
+        }
     }
 }
--- Task1CS/Classes/Triangle.cs
+++ Task1CS/Classes/Triangle.cs
@@ -1,63 +1,52 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using Task1CS.Interfaces;
 
 namespace Task1CS.Classes
 {
-    public class Triangle : IShape
-    {
-        private Point _pointA;
-        private Point _pointB;
-        private Point _pointC;
-        
-        //TODO: change location of this function, as it will be necessary for other classes.
-        double getLineLength(Point startPoint, Point endPoint)
-        {
-            return Math.Sqrt(Math.Pow(startPoint.X - endPoint.X, 2) + Math.Pow(startPoint.Y - endPoint.Y, 2));
-        }
+	public class Triangle : IShape
+	{
+		private Point _pointA;
+		private Point _pointB;
+		private Point _pointC;
 
-        public double CalcPerimeter()
-        {
-            return getLineLength(_pointA, _pointB) + getLineLength(_pointB, _pointC) + getLineLength(_pointC, _pointA);
-        }
+		public double CalcPerimeter()
+		{
+			return Point.CalcDistance(_pointA, _pointB) + 
+				   Point.CalcDistance(_pointB, _pointC) + 
+				   Point.CalcDistance(_pointC, _pointA);
+		}
 
-        public double CalcSquare()
-        {
-            var halfPerimeter = CalcPerimeter() / 2;
-            return Math.Sqrt(halfPerimeter * (halfPerimeter - getLineLength(_pointA, _pointB))
-                                           * (halfPerimeter - getLineLength(_pointB, _pointC))
-                                           * (halfPerimeter - getLineLength(_pointC, _pointA)));
-        }
+		public double CalcSquare()
+		{
+			var halfPerimeter = CalcPerimeter() / 2;
+			return Math.Sqrt(halfPerimeter * (halfPerimeter - Point.CalcDistance(_pointA, _pointB))
+										   * (halfPerimeter - Point.CalcDistance(_pointB, _pointC))
+										   * (halfPerimeter - Point.CalcDistance(_pointC, _pointA)));
+		}
 
+		public void ReadFromFile(ref StreamReader streamReader)
+		{
+			var line = streamReader.ReadLine();
+			if (line == null)
+			{
+				throw new IOException("can't read data from stream");
+			}
+			var data = line.Split(' ');
 
-        public void ReadFromFile(ref StreamReader streamReader)
-        {
-            var line = streamReader.ReadLine();
+			_pointA = new Point(data[1], data[2]);
+			_pointB = new Point(data[3], data[4]);
+			_pointC = new Point(data[5], data[6]);
+	
+		}
 
-            if (line == null)
-            {
-                throw new Exception("can't read data from stream");
-                // TODO: implement StreamReaderException
-            }
-            
-            var data = line.Split(' ');
-
-            _pointA.X = Convert.ToDouble(data[1]);
-            _pointA.Y = Convert.ToDouble(data[2]);
-
-            _pointB.X = Convert.ToDouble(data[3]);
-            _pointB.Y = Convert.ToDouble(data[4]);
-
-            _pointC.X = Convert.ToDouble(data[5]);
-            _pointC.Y = Convert.ToDouble(data[6]);
-        }
-
-        public void WriteToFile(ref StreamWriter streamWriter)
-        {
-            streamWriter.WriteLine("Triangle :");
-            streamWriter.WriteLine($"A({_pointA.X}, {_pointA.Y})");
-            streamWriter.WriteLine($"B({_pointB.X}, {_pointB.Y})");
-            streamWriter.WriteLine($"C({_pointC.X}, {_pointC.Y})");
-        }
-    }
+		public void WriteToFile(ref StreamWriter streamWriter)
+		{
+			streamWriter.WriteLine("Triangle :");
+			streamWriter.WriteLine($"A({_pointA.X}, {_pointA.Y})");
+			streamWriter.WriteLine($"B({_pointB.X}, {_pointB.Y})");
+			streamWriter.WriteLine($"C({_pointC.X}, {_pointC.Y})");
+		}
+	}
 }
