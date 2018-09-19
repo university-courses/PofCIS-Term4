2ea2e8e380d8d3ef383cccc1544d3df8273130a8
oshipka <olenkashipka@gmail.com>
2018-09-19 23:35:37 +0300

    added xml documentation for IShape interface

--- Task1CS/Interfaces/IShape.cs
+++ Task1CS/Interfaces/IShape.cs
@@ -4,10 +4,26 @@ using Task1CS.Classes;
 
 namespace Task1CS.Interfaces
 {
+	/// <inheritdoc />
+	/// <summary>
+	/// Interface for shapes. 
+	/// </summary>
 	public interface IShape : IParsable
 	{
+		/// <summary>
+		/// Function used to calculate shape's square.
+		/// </summary>
+		/// <returns>Square of the shape.</returns>
 		double CalcSquare();
+		/// <summary>
+		/// Function used to calculate shape's perimeter.
+		/// </summary>
+		/// <returns>Perimeter of shape.</returns>
 		double CalcPerimeter();
+		/// <summary>
+		/// Function used to get shape's points.
+		/// </summary>
+		/// <returns>Array of points.</returns>
 		IEnumerable<Point> GetPoints();
 	}
 }
