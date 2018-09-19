452b5ceeab92b2c276f6326237bbe0c7949a0162
oshipka <olenkashipka@gmail.com>
2018-09-20 00:27:20 +0300

    added xml documentation for IParsable interface

--- Task1CS/Interfaces/IParsable.cs
+++ Task1CS/Interfaces/IParsable.cs
@@ -1,7 +1,15 @@
 namespace Task1CS.Interfaces
 {
+	/// <summary>
+	/// Interface to allow to parse strings of data
+	/// </summary>
 	public interface IParsable
 	{
+		/// <summary>
+		/// Function parses strings of data. 
+		/// </summary>
+		/// <param name="data">String that has to be parsed.</param>
+		/// <returns>True if string was parsed, false if string was not parsed.</returns>
 		bool Parse(string data);
 	}
 }
