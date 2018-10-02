2b7d12c4e4ac715768286be429d85a6d33b27912
oshipka <olenkashipka@gmail.com>
2018-10-02 23:41:35 +0300

    add Util class: GetHexagonByClick, PointIsInRect(need the implementation) and GetHexagonIdByName static methods

---
+++ DrawShape/Utils/Util.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+using DrawShape.Classes;
+
+namespace DrawShape.Utils
+{
+	public static class Util
+	{
+		public static Hexagon GetHexagonByClick(Point clickPosition, List<Hexagon> hexagons)
+		{
+			for (var i = hexagons.Count - 1; i >= 0; i--)
+			{
+				if (PointIsInRect(clickPosition, hexagons[i]))
+				{
+					return hexagons[i];
+				}
+			}
+
+			return null;
+		}
+
+		public static bool PointIsInRect(Point point, Hexagon rect)
+		{
+			// TODO: check if mouse is clicked on hexagon
+			
+			return false;
+		}
+		
+		/// <summary>
+		/// Returns an index of a hexagon with specified name.
+		/// </summary>
+		/// <param name="name">Hexagon's name.</param>
+		/// <param name="hexagons">A list of available hexagons.</param>
+		/// <returns>Index of hexagon.</returns>
+		/// <exception cref="InvalidDataException">Throws if hexagon does not exist.</exception>
+		public static int GetHexagonIdByName(string name, List<Hexagon> hexagons)
+		{
+			for (var i = 0; i < hexagons.Count; i++)
+			{
+				if (hexagons[i].Name == name)
+				{
+					return i;
+				}
+			}
+
+			throw new InvalidDataException("hexagon does not exist");
+		}
+	}
+}
