85582fe7b9eef5f03769d73116496a94201059ff
oshipka <olenkashipka@gmail.com>
2018-10-02 21:48:19 +0300

    Implemented Hexagon class, added Point structure

--- DrawShape/Classes/Hexagon.cs
+++ DrawShape/Classes/Hexagon.cs
@@ -1,7 +1,49 @@
-﻿namespace DrawShape.Classes
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace DrawShape.Classes
 {
+	[Serializable]
 	public class Hexagon
 	{
+		[XmlAttribute]
+		public Point[] Points { get; set; }
+
+		[XmlAnyAttribute]
+		public int R { get; set; }
 		
+		[XmlAnyAttribute]
+		public int G { get; set; }
+		
+		[XmlAnyAttribute]
+		public int B { get; set; }
+		
+		[XmlAttribute]
+		public string Name { get; set; }
+
+		public Hexagon(string name, List<Point> points, Color color)
+		{
+			Name = name;
+			Points = points.ToArray();
+			R = color.R;
+			G = color.G;
+			B = color.B;
+		}
+
+		public Polygon ToPolygon()
+		{
+			var polygon = new Polygon();
+			foreach (var point in Points)
+			{
+				polygon.Points.Add(new System.Windows.Point(point.X, point.Y));
+			}
+			var color = Color.FromRgb((byte)R, (byte)G, (byte)B);
+			polygon.Stroke = new SolidColorBrush(color);
+			polygon.Fill = new SolidColorBrush(color);
+			return polygon;
+		}
 	}
 }
---
+++ DrawShape/Classes/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace DrawShape.Classes
+{
+	[Serializable]
+	public struct Point
+	{
+		[XmlAttribute]
+		public double X { get; set; }
+		
+		[XmlAttribute]
+		public double Y { get; set; }
+	}
+}
