4afca3be2ffb18cbeed0032ac387cd0b6dbe83a1
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-10-02 14:53:20 +0300

    add docs for Serialization class, minor changes in UI, implement save/open file logic

--- DrawShape/MainWindow.xaml.cs
+++ DrawShape/MainWindow.xaml.cs
@@ -1,43 +1,88 @@
 ﻿using System.Windows;
+using System.Collections.Generic;
+
+using Microsoft.Win32;
+
+using DrawShape.Classes;
+using DrawShape.Utils;
 
 namespace DrawShape
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
 	{
-        /// <summary>
-        /// Constructs the main window of an application.
-        /// </summary>
+		public List<Hexagon> Hexagons { get; private set; }
+		private bool _isSaved;
+		
+		/// <summary>
+		/// Constructs the main window of an application.
+		/// </summary>
 		public MainWindow()
 		{
 			InitializeComponent();
+			Hexagons = new List<Hexagon>();
 		}
 
-        private void FileMenu_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
+		private void NewButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (!_isSaved)
+			{
+				// TODO: open saving dialog.
+			}
+			
+			// TODO: refresh drawing panel.
+			
+			_isSaved = false;
+			
+			MessageBox.Show("New Button Clicked");
+		}
 
-        private void NewButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("New Button Clicked");
-        }
+		private void SaveButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (Hexagons.Count > 0 && !_isSaved)
+			{
+				var sfd = new SaveFileDialog
+				{
+					Filter = @"Xml files (*.xml)|*.xml|All files (*.*)|*.*",
+					DefaultExt = "xml",
+					FileName = "Rectangles",
+					AddExtension = true
+				};
+				if (sfd.ShowDialog() == true)
+				{
+					Serialization.SerializeHexagons(sfd.FileName, Hexagons);
+					_isSaved = true;
+				}
+			}
+			
+			MessageBox.Show("Save Button Clicked");
+		}
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Save Button Clicked");
-        }
+		private void OpenButton_Click(object sender, RoutedEventArgs e)
+		{
+			var ofd = new OpenFileDialog
+			{
+				Filter = @"Xml files (*.xml)|*.xml|All files (*.*)|*.*",
+				DefaultExt = "xml",
+				AddExtension = true
+			};
 
-        private void OpenButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Open Button Clicked");
-        }
+			if (ofd.ShowDialog() == true)
+			{
+				Hexagons = Serialization.DeserializeHexagons(ofd.FileName);
+				
+				// TODO: draw deserialized hexagons.
+				
+				_isSaved = true;
+			}
+			MessageBox.Show("Open Button Clicked");
+		}
 
-        private void ShapesMenu_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-    }
+		private void ShapesMenu_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Shapes Manu Clicked");
+		}
+	}
 }
--- DrawShape/Utils/Serialization.cs
+++ DrawShape/Utils/Serialization.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Xml.Serialization;
+using System.Collections.Generic;
+
 using DrawShape.Classes;
 
 namespace DrawShape.Utils
 {
+	/// <summary>
+	/// Static class for hexagon serialization.
+	/// </summary>
 	public static class Serialization
 	{
+		/// <summary>
+		/// Serializes a list of hexagons to a file.
+		/// </summary>
+		/// <param name="fileName">Path to target file.</param>
+		/// <param name="hexagons">A list of hexagons to serialize.</param>
 		public static void SerializeHexagons(string fileName, List<Hexagon> hexagons)
 		{
 			Stream stream = new FileStream(fileName, FileMode.Create);
@@ -14,6 +23,11 @@ namespace DrawShape.Utils
 			stream.Close();
 		}
 
+		/// <summary>
+		/// Deserializes hexagons to a list.
+		/// </summary>
+		/// <param name="fileName">Path to source file.</param>
+		/// <returns>A list of deserialized hexagons.</returns>
 		public static List<Hexagon> DeserializeHexagons(string fileName)
 		{
 			Stream stream = new FileStream(fileName, FileMode.Open);
