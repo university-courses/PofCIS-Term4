9901647019ac6b39124979bfa883ac9b945b6015
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-10-02 14:17:07 +0300

    add hexagon serialization

--- DrawShape/MainWindow.xaml.cs
+++ DrawShape/MainWindow.xaml.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace DrawShape
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
 	{
         /// <summary>
         /// Constructs the main window of an application.
@@ -30,32 +17,27 @@ namespace DrawShape
 
         private void FileMenu_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void NewButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("New Button Clicked");
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Save Button Clicked");
         }
 
         private void OpenButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Open Button Clicked");
         }
 
         private void ShapesMenu_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void SaveMenu_Click(object sender, RoutedEventArgs e)
-        {
-
+            
         }
     }
 }
---
+++ DrawShape/Utils/Serialization.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using DrawShape.Classes;
+
+namespace DrawShape.Utils
+{
+	public static class Serialization
+	{
+		public static void SerializeHexagons(string fileName, List<Hexagon> hexagons)
+		{
+			Stream stream = new FileStream(fileName, FileMode.Create);
+			new XmlSerializer(typeof(List<Hexagon>)).Serialize(stream, hexagons);
+			stream.Close();
+		}
+
+		public static List<Hexagon> DeserializeHexagons(string fileName)
+		{
+			Stream stream = new FileStream(fileName, FileMode.Open);
+			var result = new XmlSerializer(typeof(List<Hexagon>)).Deserialize(stream) as List<Hexagon>;
+			stream.Close();
+			return result;
+		}
+	}
+}
