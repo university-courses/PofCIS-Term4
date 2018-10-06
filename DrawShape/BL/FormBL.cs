64754c6399e0da886129a02158eb8298644f4f27
oshipka <olenkashipka@gmail.com>
2018-10-06 19:26:16 +0300

    add xml documentation

--- DrawShape/BL/FormBL.cs
+++ DrawShape/BL/FormBL.cs
@@ -10,8 +10,16 @@ using DrawShape.Classes;
 
 namespace DrawShape.BL
 {
+	/// <summary>
+	/// Class for business logick of the application.
+	/// </summary>
 	public static class FormBl
 	{
+		/// <summary>
+		/// Function to pick a new colour.
+		/// </summary>
+		/// <param name="current">Parameter where the picked colour will be stored.</param>
+		/// <param name="picker">Rectangle where picked colour will be displayed.</param>
 		public static void SetColor(ref Brush current, ref Rectangle picker)
 		{
 			if (!Util.GetColorFromColorDilog(out var color))
@@ -23,12 +31,20 @@ namespace DrawShape.BL
 			picker.Fill = color;
 		}
 
+		/// <summary>
+		/// Function to read hexagons from xml file.
+		/// </summary>
+		/// <returns>Hexagons read.</returns>
 		public static IEnumerable<Hexagon> ReadHexagons()
 		{
 			var ofd = new OpenFileDialog { Filter = @"Xml files (*.xml)|*.xml", DefaultExt = "xml", AddExtension = true};
 			return ofd.ShowDialog() == true ? Serialization.DeserializeHexagons(ofd.FileName) : null;
 		}
 
+		/// <summary>
+		/// Function to save hexagons to xml file.
+		/// </summary>
+		/// <param name="canvas">Canvas from which hexagons are to be saved.</param>
 		public static void SaveHexagons(ref Canvas canvas)
 		{
 			var sfd = new SaveFileDialog {Filter = @"Xml files (*.xml)|*.xml", DefaultExt = "xml", FileName = "Rectangles", AddExtension = true};
--- DrawShape/Classes/Point.cs
+++ DrawShape/Classes/Point.cs
@@ -4,20 +4,34 @@ using System.Xml.Serialization;
 
 namespace DrawShape.Classes
 {
+	/// <summary>
+	/// Class to represent a point in two-dimensional space.
+	/// </summary>
 	[Serializable]
 	public class Point
 	{
+        /// <summary>
+        /// X coordinate of a point.
+        /// </summary>
 		[XmlAttribute]
 		public double X { get; set; }
 		
+        /// <summary>
+        /// Y coordinate of a point.
+        /// </summary>
 		[XmlAttribute]
 		public double Y { get; set; }
 
 		public Point()
 		{
 		}
-		
-		public Point(double x, double y)
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Point"/> class.  
+        /// </summary>
+        /// <param name="x">X coordinate of a point.</param>
+        /// <param name="y">Y coordinate of a point.</param>
+        public Point(double x, double y)
 		{
 			X = x;
 			Y = y;
