df02ee0d4736c66b19159cae2103e092b78d6b27
dana_hrechyn <dana.hrechyn@gmail.com>
2018-10-10 21:08:16 +0300

    Fixed part of XML documentation

--- DrawShape/Utils/Util.cs
+++ DrawShape/Utils/Util.cs
@@ -94,16 +94,16 @@ namespace DrawShape.Utils
 				return true;
 
 			// Special Cases 
-			// p1, q1 and p2 are colinear and p2 lies on segment p1q1 
+			// p1, q1 and p2 are collinear and p2 lies on segment p1q1 
 			if (o1 == 0 && OnSegment(firstSidePointOne, secondSidePointOne, firstSidePointTwo)) return true;
 
-			// p1, q1 and p2 are colinear and q2 lies on segment p1q1 
+			// p1, q1 and p2 are collinear and q2 lies on segment p1q1 
 			if (o2 == 0 && OnSegment(firstSidePointOne, secondSidePointTwo, firstSidePointTwo)) return true;
 
-			// p2, q2 and p1 are colinear and p1 lies on segment p2q2 
+			// p2, q2 and p1 are collinear and p1 lies on segment p2q2 
 			if (o3 == 0 && OnSegment(secondSidePointOne, firstSidePointOne, secondSidePointTwo)) return true;
 
-			// p2, q2 and q1 are colinear and q1 lies on segment p2q2 
+			// p2, q2 and q1 are collinear and q1 lies on segment p2q2 
 			if (o4 == 0 && OnSegment(secondSidePointOne, firstSidePointTwo, secondSidePointTwo)) return true;
 
 			return false;
