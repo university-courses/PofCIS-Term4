cd1fc30f2947f1f5e4d0baf982d596d3d1b97290
dana_hrechyn <dana.hrechyn@gmail.com>
2018-10-10 11:34:04 +0300

    Created Constructor Data for Util tests.

---
+++ DrawShape.Test/Utils/UtilTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+using System;
+using System.IO;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Windows.Shapes;
+using DrawShape.Utils;
+using DrawShape.Classes;
+
+namespace DrawShape.Test.Utils
+{
+    public class UtilTest
+    {
+        [Theory]
+        [MemberData(nameof(ConstructorData.SuccessData), MemberType = typeof(ConstructorData))]
+        public void TestPointIsInHexagon(Point point, Polygon hexagon)
+        {
+            
+        }
+
+        private class ConstructorData
+        {
+            public static IEnumerable<object[]> SuccessData => new List<object[]>
+            {
+                new object[]
+                {
+                   new Point(0, 0), new Polygon()
+                },
+                
+            };
+        }
+    }
+}
