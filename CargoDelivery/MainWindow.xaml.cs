0e285a4e8618a57b7d1a3a6549b004b0f2234331
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-11-06 10:42:39 +0200

    Minor UI fix

--- CargoDelivery/MainWindow.xaml.cs
+++ CargoDelivery/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Collections.Generic;
 using System.Windows.Media.Effects;
+
 using CargoDelivery.BL;
 using CargoDelivery.Classes;
 
