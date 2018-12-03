2642203aa041449f595ffa5fceea76f0960fc7d0
seleman.a <31805806+selemanka@users.noreply.github.com>
2018-12-03 23:00:18 +0200

    Update OrderContext.cs

--- CargoDelivery/DAL/OrderContext.cs
+++ CargoDelivery/DAL/OrderContext.cs
@@ -4,13 +4,33 @@ using CargoDelivery.Classes;
 using CargoDelivery.Classes.OrderData;
 
 namespace CargoDelivery.DAL
-{
+{       /// <summary>
+	/// Represents an order.
+	/// </summary>
 	public class OrderContext : DbContext
-	{
+	{	/// <summary>
+		/// Holds orders.
+		/// </summary>
 		public DbSet<Order> Orders { get; set; }
+		
+		/// <summary>
+		/// Contains client personal information.
+		/// </summary>
 		public DbSet<ClientData> ClientDatas { get; set; }
+		
+		/// <summary>
+		/// Holds an information about ordered goods.
+		/// </summary>
 		public DbSet<GoodsData> GoodsDatas { get; set; }
+		
+		/// <summary>
+		/// Represents shop data.
+		/// </summary>
 		public DbSet<ShopData> ShopDatas { get; set; }
+		
+		/// <summary>
+		/// Contains addresses.
+		/// </summary>
 		public DbSet<Address> Addresses { get; set; }
 
 		public OrderContext() : base("CargoDeliveryDb")
